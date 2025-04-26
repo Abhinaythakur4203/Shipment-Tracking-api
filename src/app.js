99dc1eabe70a482a12c48a0062558e47f0947539
AbhinayThakur <abhinay4203@gmail.com>
2025-04-26 22:02:58 +0530

    Add initial project structure with essential dependencies and successful DB connection

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+});
+
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export { app };
